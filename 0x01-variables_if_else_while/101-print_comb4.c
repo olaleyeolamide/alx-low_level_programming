b65bdbd8bcb8bd5832e54af5788537ac4e3bf193
olaleyeolamide <olaleyeolamide999@gmail.com>
2023-02-17 15:50:53 -0800

    print combinaton 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - print diferent combinations of three digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+	int d;
+	int e = 0;
+
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				putchar('0' + c);
+				putchar('0' + d);
+				putchar('0' + e);
+
+				if (c + d + e != 9 + 8 + 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+			c++;
+
+		}
+		d++;
+
+	}
+	e++;
+
+}
+putchar('\n');
+return (0);
