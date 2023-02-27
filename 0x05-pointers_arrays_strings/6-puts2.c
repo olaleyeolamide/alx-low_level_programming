9861b5b2ec5437c569ea5eaa2197a7591846032e
olaleyeolamide <olaleyeolamide999@gmail.com>
2023-02-27 05:40:06 -0800

    mend

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,18 +3,24 @@
 
 /**
  * puts2 - print every other character of a string
- * @*str: string to be printed
+ * @str: string to be printed
  * Return: void
  */
 
 void puts2(char *str)
 
 {
-	char *str[];
+	int i;
+	int j = 0;
 
-	for (; *str != '\0'; *str = +2)
+	while (str[j] != '\0')
 	{
-		_putchar(*str);
+		j++;
+	}
+	
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 
