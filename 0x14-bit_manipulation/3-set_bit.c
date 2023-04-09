196fe3b00b7b4a6002580e01ac8ad49e55acd2db
olaleyeolamide <olaleyeolamide999@gmail.com>
2023-04-09 07:01:07 -0700

    mend

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,19 +5,16 @@
  * set_bit - set value of bit to 1
  * @n: number to evaluate
  * @index: given index
- * Return: set value
+ * Return: 1
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int n;
-	int set_val;
-	int index;
+	if (index > 63)
+		return (-1);
 
-	if (index < 64)
-		set_val = 1;
-
-	return (set_val);
+	*n = (1UL << index | *n);
+		return (1);
 
 }
 
